465e984db228b4875c1af6e5d261749dc776c6c3
Javier <javiergaspar07@gmail.com>
2023-04-26 16:27:15 -0400

    endpoint removed

--- books/views.py
+++ books/views.py
@@ -15,26 +15,9 @@ from books.serializers import BookSerializer
 
 # Utils
 from utilities.functions.charge_books import charge_books
-
-
-class AddBooks(views.APIView):
-    def get(self, *args, **kwargs):
-        #try:
-        charge_books()
-        body = 'Books charged successfully'
-        status_code = 201
-        """except:
-            status_code = 400
-            body = 'Books charge failed'"""
-        
-        return Response(body, status=status_code)
     
     
-class BooksViewSet(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.ListModelMixin,
-                    mixins.DestroyModelMixin,
-                    viewsets.GenericViewSet):
+class BooksViewSet(viewsets.ModelViewSet):
     """Circle view set."""
     
     serializer_class = BookSerializer
@@ -42,3 +25,14 @@ class BooksViewSet(mixins.RetrieveModelMixin,
     lookup_field = 'title'
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]   
     filter_fields = ('author')
+
+    def create(self, **kwargs):
+        try:
+            charge_books()
+            body = 'Books charged successfully'
+            status_code = 201
+        except:
+            status_code = 400
+            body = 'Books charge failed'
+        
+        return Response(body, status=status_code)
