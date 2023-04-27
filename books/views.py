f5eec0a58ba93aa7db508c96207b78887abfc5aa
Javier <javiergaspar07@gmail.com>
2023-04-27 14:56:12 -0400

    'create' method updated to manage books creation better

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
 """Circles API Views"""
 
 # Django REST Framework
-from rest_framework import viewsets, mixins, views
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 # Filters
@@ -18,20 +18,21 @@ from utilities.functions.charge_books import charge_books
     
     
 class BooksViewSet(viewsets.ModelViewSet):
-    """Circle view set."""
+    """Books view set."""
     
     serializer_class = BookSerializer
     queryset = Book.objects.all()
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]   
     filter_fields = ('author, title')
 
-    def create(self, **kwargs):
+    def create(self, request, *args, **kwargs):
+        """
+        Manage Books creation process.
+        """
         try:
-            charge_books()
-            body = 'Books charged successfully'
-            status_code = 201
-        except:
-            status_code = 400
-            body = 'Books charge failed'
-        
-        return Response(body, status=status_code)
+            books_data = charge_books()
+        except Exception as e:
+            return Response(e.__str__(), status=status.HTTP_400_BAD_REQUEST)
+
+        response = {"detail": f"{len(books_data)} Books successfully charged."}
+        return Response(response, status=status.HTTP_201_CREATED)
